486e68502af4db2d08065fd446b55848981cf217
root <demirk1776@gmail.com>
2021-01-20 01:04:11 +0000

    guest book done

--- _02_lists/_b_guest_book.py
+++ _02_lists/_b_guest_book.py
@@ -9,6 +9,26 @@ class GuestBook(tk.Tk):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.guestNameList = list()
+        self.newGuestName = ""
+        self.finalString = str()
+
+        addGuestButton = tk.Button(self, text='Add Guest', bg='White', fg='Black', command=lambda: self.addGuestButton_pressed())
+        addGuestButton.place(x=0, y=150)
+        viewGuestsButton = tk.Button(self, text='View Guests', bg='White', fg='Black', command=lambda: self.viewGuestsButton_pressed())
+        viewGuestsButton.place(x=100, y=150)
+
+    def addGuestButton_pressed(self):
+        self.newGuestName = simpledialog.askstring(title="New Guest", prompt="Enter Guest Name")
+        if self.newGuestName is not None:
+            self.guestNameList.append(self.newGuestName)
+
+    def viewGuestsButton_pressed(self):
+        self.finalString = ""
+        for i in range(len(self.guestNameList)):
+            self.finalString = self.finalString + "\n" + str(i+1) + " " + self.guestNameList[i]
+        messagebox.showinfo(title="Guest List", message=self.finalString)
+
 
 if __name__ == '__main__':
     # 1. Make a new GuestBook
